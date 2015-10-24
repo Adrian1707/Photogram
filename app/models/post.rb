3060fcfd2a8d910e8f614ce369135c6cf61b27fa
Adrian Booth <Adrian.booth17@gmail.com>
2015-10-24 15:18:44 +0100

    Add time_ago method to each picture

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,5 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_attached_file :image, :styles => { :medium => "640x"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_length_of :caption, :minimum => 3, :maximum => 300
 end
