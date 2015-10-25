bb954a7171dad34f197f9a9574afd4198173c785
Adrian Booth <Adrian.booth17@gmail.com>
2015-10-25 10:58:16 +0000

    User can only edit their own profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
   def show
+    @user = User.find_by(user_name: params[:user_name])
     @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
   end
 
@@ -9,7 +10,21 @@ class ProfilesController < ApplicationController
   end
 
   def update
+    @user = User.find_by(user_name: params[:user_name])
+    if @user.update(profile_params)
+      flash[:success] = 'Your profile has been updated.'
+      redirect_to profile_path(@user.user_name)
+    else
+      @user.errors.full_messages
+      flash[:error] = @user.errors.full_messages
+      render :edit
+    end
+  end
 
-  end 
+  private
+
+  def profile_params
+    params.require(:user).permit(:avatar, :bio)
+  end
 
 end
