113b0c64339015bf8db23b53c9ceb70bb651a7dc
Adrian Booth <Adrian.booth17@gmail.com>
2015-10-25 10:22:06 +0000

    Profile page added

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,15 @@
+class ProfilesController < ApplicationController
+  def show
+    @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
+  end
+
+  def edit
+    @user = User.find_by(user_name: params[:user_name])
+    @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
+  end
+
+  def update
+
+  end 
+
+end
