ce982c32b2c8329f09ce1f4c177e7e92c6503aa3
Adrian Booth <Adrian.booth17@gmail.com>
2015-10-25 12:14:47 +0000

    Profile pages styles further

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,16 +1,16 @@
 class ProfilesController < ApplicationController
+    before_action :owned_profile, only: [:edit, :update]
+    before_action :set_user, only: [:show, :edit, :update]
+
   def show
-    @user = User.find_by(user_name: params[:user_name])
     @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
   end
 
   def edit
-    @user = User.find_by(user_name: params[:user_name])
     @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
   end
 
   def update
-    @user = User.find_by(user_name: params[:user_name])
     if @user.update(profile_params)
       flash[:success] = 'Your profile has been updated.'
       redirect_to profile_path(@user.user_name)
@@ -25,6 +25,18 @@ class ProfilesController < ApplicationController
 
   def profile_params
     params.require(:user).permit(:avatar, :bio)
+  end
+
+  def set_user
+    @user = User.find_by(user_name: params[:user_name])
+  end
+
+  def owned_profile
+    @user = User.find_by(user_name: params[:user_name])
+    unless @user == current_user
+      flash[:alert] = "That profile doesn't belong to you!"
+      redirect_to root_path
+    end
   end
 
 end
